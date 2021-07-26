ae17b8933e445ddd72f6d689c36f70befb0b80c9
hako <mhsatman@gmail.com>
2021-07-26 13:18:54 +0300

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ga "0.1.0-SNAPSHOT"
   :description "Genetic algorithms library"
   :url "http://example.com/FIXME"
-  :license {:name MIT"
+  :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot org.expr.ga.core
