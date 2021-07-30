229d812ca46fd955a5194afaa376485832803b56
jbytecode <mhsatman@gmail.com>
2021-07-30 13:47:45 +0300

    add functions for expression manipulation

--- src/org/expr/ga/expression.clj
+++ src/org/expr/ga/expression.clj
@@ -17,6 +17,17 @@
 
 (def constants (range 10))
 
+
+(defn find-function-by-name [name functions]
+  (first (filter #(= name (:func %1)) functions)))
+
+(defn arity-by-name [name functions]
+  (:p (find-function-by-name name functions)))
+
+(defn functions-of-arity [arity functions]
+  (filter #(= arity (:p %1)) functions))
+
+
 (defn random-expression [functions constants]
   (let
    [func      (rand-nth functions)
@@ -42,4 +53,30 @@
                              (< p 4/4) expr)))))
 
 
+
+(defn mutate-fn-with [expr functions]
+  (let
+   [op                 (first expr)
+    op-fn-entry        (find-function-by-name op functions)
+    arity              (:p op-fn-entry)
+    potential-fns      (functions-of-arity arity functions)
+    lucky-fn           (rand-nth potential-fns)
+    lucky-fn-op        (:func lucky-fn)
+    new-expr           (cons lucky-fn-op (for [e (rest expr)] e))] new-expr))
+
+(defn mutate-args-with [expr functions constants]
+  (let
+   [r           (rand)]
+    (cond
+      (not (seq? expr))   (rand-nth constants)
+      (< r 0.50)   expr
+      (< r 1.0)   (cons (first expr)
+                        (for [e (rest expr)]
+                          (mutate-args-with
+                           (random-expression functions constants) functions constants))))))
+
+(defn mutate-with [expr functions constants]
+  (if (< (rand) 0.5)
+    (mutate-fn-with expr functions)
+    (mutate-args-with expr functions constants)))
 
--- test/org/expr/ga/expression_test.clj
+++ test/org/expr/ga/expression_test.clj
@@ -16,3 +16,25 @@
         (= sexpr '(- 8 4))
         (= sexpr '(* 5 10))
         (= sexpr expr))))))
+
+
+(deftest test-find-function-by-name
+  (testing "Find function by name"
+    (is (=  {:func 'div :p 2} (e/find-function-by-name 'div e/functions)))
+    (is (=  {:func 'sqrt :p 1} (e/find-function-by-name 'sqrt e/functions)))
+    (is (=  {:func '+ :p 2} (e/find-function-by-name '+ e/functions)))))
+
+(deftest test-arity-by-name
+  (testing "Arity by name"
+    (is (=  2 (e/arity-by-name '+ e/functions)))
+    (is (=  2 (e/arity-by-name 'div e/functions)))
+    (is (=  1 (e/arity-by-name 'sqrt e/functions)))))
+
+(deftest test-functions-of-arity
+  (testing "Functions of arity"
+    (let
+     [fns1            (e/functions-of-arity 1 e/functions)
+      fns2            (e/functions-of-arity 2 e/functions)]
+      ; TODO
+      ; Complete this test
+      )))
